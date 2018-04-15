03066663497df3c4ac180250b9b2789f2757b2a7
expexes <devid.kohut@gmail.com>
2018-04-16 00:48:21 +0300

    oops

--- src/Solovey/Database/Methods/Pagination.php
+++ src/Solovey/Database/Methods/Pagination.php
@@ -16,7 +16,6 @@ use function a2o;
 use function array_push;
 use function ceil;
 use function criteriaToSQL;
-use function r;
 
 class Pagination
 {
@@ -75,8 +74,6 @@ class Pagination
 		$limits = "LIMIT $this->perPage OFFSET $offset";
 
 		$critQL = criteriaToSQL($this->criteria);
-
-		r("$query WHERE {$critQL['query']} $limits");
 
 		if (empty($this->criteria))
 			$result = Database::query("$query $limits")->execute()->fetchAll();
